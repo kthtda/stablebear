3c1f543149d159dbf4780ff063d75ab21947cc84
Bjorn Wehlin <bjorn.wehlin@gmail.com>
2026-06-23 15:15:04 +0000

    test: assert self-aliasing assignment matches numpy in the int/control cases

--- test/python/test_bugscan_setitem.py
+++ test/python/test_bugscan_setitem.py
@@ -38,21 +38,28 @@ def test_forward_overlap_1d():
 def test_int_tensor_self_alias():
     a = sb.IntTensor(np.array([1, 2, 3, 4], dtype=np.int64))
     a[:] = a[::-1]
-    assert np.asarray(a).tolist() == [4, 3, 2, 1]
+    na = np.array([1, 2, 3, 4], dtype=np.int64)
+    na[:] = na[::-1]
+    assert np.asarray(a).tolist() == na.tolist() == [4, 3, 2, 1]
 
 
 def test_copy_rhs_control_still_correct():
     c = sb.FloatTensor(np.array([1.0, 2.0, 3.0, 4.0], dtype=np.float32))
     c[:] = c[::-1].copy()
-    assert np.asarray(c).tolist() == [4.0, 3.0, 2.0, 1.0]
+    nc = np.array([1.0, 2.0, 3.0, 4.0], dtype=np.float32)
+    nc[:] = nc[::-1].copy()
+    assert np.asarray(c).tolist() == nc.tolist() == [4.0, 3.0, 2.0, 1.0]
 
 
 def test_non_aliased_assignment_unchanged():
     d = sb.FloatTensor(np.array([1.0, 2.0, 3.0, 4.0]))
     e = sb.FloatTensor(np.array([10.0, 20.0, 30.0, 40.0]))
     d[:] = e[::-1]
-    assert np.asarray(d).tolist() == [40.0, 30.0, 20.0, 10.0]
-    assert np.asarray(e).tolist() == [10.0, 20.0, 30.0, 40.0]  # source untouched
+    nd = np.array([1.0, 2.0, 3.0, 4.0])
+    ne = np.array([10.0, 20.0, 30.0, 40.0])
+    nd[:] = ne[::-1]
+    assert np.asarray(d).tolist() == nd.tolist() == [40.0, 30.0, 20.0, 10.0]
+    assert np.asarray(e).tolist() == ne.tolist() == [10.0, 20.0, 30.0, 40.0]  # source untouched
 
 
 def test_partial_overlap_disjoint_halves():
